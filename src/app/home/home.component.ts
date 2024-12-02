b146e3dd87556b920c495917cdca9493925d0c1d
ameda-kevin <amedakevin@gmail.com>
2024-12-03 01:13:32 +0300

    done recap with other modules and logic

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component,signal } from '@angular/core';
+import { GreetingComponent } from '../components/greeting/greeting.component';
+import { CounterComponent } from '../components/counter/counter.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [GreetingComponent,CounterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  name = signal('Kevin Ameda Kisevu');
+  message = signal('Ola! Gracious');
+
+
+  keyUpHandler(event: KeyboardEvent){
+    console.log(`User pressed the ${event.key} key`);
+  }
+}
