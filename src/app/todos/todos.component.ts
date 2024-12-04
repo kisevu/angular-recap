f495ac4b72d5c666f5b1cfb82e48860ec0c4910c
ameda-kevin <amedakevin@gmail.com>
2024-12-04 14:56:11 +0300

    working with Angular pipes for data transformation in the template and use of custom pipes plus Angular built-in pipes like the CurrencyPipe, UpperCasePipe etc

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -3,11 +3,13 @@ import { TodosService } from '../services/todos.service';
 import { Todo } from '../model/todo.type';
 import { catchError } from 'rxjs';
 import { TodoItemComponent } from '../components/todo-item/todo-item.component';
+import {FormsModule} from '@angular/forms';
+import { FilterTodosPipe } from '../pipes/filter-todos.pipe';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [TodoItemComponent], 
+  imports: [TodoItemComponent,FormsModule, FilterTodosPipe], 
   templateUrl: './todos.component.html', 
   styleUrl: './todos.component.scss',
 })
@@ -20,6 +22,8 @@ export class TodosComponent implements OnInit{
   there are two steps to that
  */
   todoItems = signal<Array<Todo>>([]);
+
+  searchTerm = signal('');
 
 
 
